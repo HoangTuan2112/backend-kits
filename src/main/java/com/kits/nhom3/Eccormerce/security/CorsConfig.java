e0118b37e2c6fed378a4382d395109fb42992f9b
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2024-09-26 09:26:14 +0700

    deploy

--- src/main/java/com/kits/nhom3/Eccormerce/security/CorsConfig.java
+++ src/main/java/com/kits/nhom3/Eccormerce/security/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://ecommerce2002-e023e02c0f58.herokuapp.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                .allowedOrigins("https://ecommerce2002-e023e02c0f58.herokuapp.com") // Chỉ cho phép các yêu cầu từ frontend của bạn
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Cho phép các phương thức HTTP
+                .allowedHeaders("*") // Cho phép tất cả các header
+                .allowCredentials(true); // Cho phép gửi cookie và xác thực
             }
         };
     }
